e0a891e5fc758be1bc50153a8ddf35513c87aa3c
ndretx <andrechteixeira@gmail.com>
2023-03-06 17:02:02 -0300

    login page

---
+++ src/components/style.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+
+export const Container = styled.div`
+   display: flex;
+   margin-top: 200px;
+   align-items: center;
+   justify-content: center;
+    
+
+`
